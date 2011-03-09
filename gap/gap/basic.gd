a15f3f3700c8a7f526a184b04e799507275ae185
lvendramin@gmail.com <lvendramin@gmail.com@a188490d-1b62-0a25-6c56-1bfc7386872d>
2011-03-09 09:42:50 +0000

    added: IsLatin

--- gap/gap/basic.gd
+++ gap/gap/basic.gd
@@ -63,6 +63,7 @@ DeclareGlobalFunction("IsConnected");
 DeclareGlobalFunction("IsGoodInvolution");
 DeclareGlobalFunction("IsBraided");
 DeclareGlobalFunction("Affinise");
+DeclareGlobalFunction("IsLatin");
 
 ##################
 ### operations ###
