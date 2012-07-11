bdd948fa06c8b83df922da3e2be11c4a3008191c
lvendramin <lvendramin@a188490d-1b62-0a25-6c56-1bfc7386872d>
2012-07-11 14:06:08 +0000

    added: wrappers functions for derivatives

--- trunk/gap/gap/derivations.gd
+++ trunk/gap/gap/derivations.gd
@@ -1,11 +1,23 @@
+################
+### Wrappers ###
+################
+DeclareGlobalFunction("Derive");
+DeclareGlobalFunction("OppositeDerive");
+DeclareGlobalFunction("IsNonZero");
+
+###################################
+### Derivatives                 ###
+### Written by Andreas Lochmann ###
+###################################
 DeclareGlobalFunction("ActNP");
 DeclareGlobalFunction("DeriveNP");
+DeclareGlobalFunction("DeriveOpNP");
 DeclareGlobalFunction("DeriveMultipleNP");
+DeclareGlobalFunction("DeriveOpMultipleNP");
 DeclareGlobalFunction("SignatureNP");
 DeclareGlobalFunction("SynBaseNP");
 DeclareGlobalFunction("NPIsFormallyZero");
 DeclareGlobalFunction("NPHasNonZeroConstant");
 DeclareGlobalFunction("NPCleanUpNilpotents");
-DeclareGlobalFunction("NPIsRecursivelyZero");
-DeclareGlobalFunction("LongestWordWithPrefix");
-DeclareGlobalFunction("LongestWord");
+DeclareGlobalFunction("NPRecursiveDerivationSequence");
+
